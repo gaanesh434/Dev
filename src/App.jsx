5b866c9c6fcbcdcb19aad755e273394caa44feb0
gaanesh434 <kkleelaganesh@gmail.com>
2025-02-09 00:10:00 +0530

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Nav from "./Components/Nav";
+import "./index.css";
 
 const App = () => {
+  const [theme, setTheme] = useState('light');
+
+  const bgColor = theme === 'light' ? 'white' : 'black';
+
   return (
-    <div>
-      <Nav />
+    <div style={{ backgroundColor: bgColor }}>
+      <Nav theme={theme} setTheme={setTheme} />
     </div>
-  )
-}
+  );
+};
+
 export default App;
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,3 +1,4 @@
+// Nav.js
 import React from "react";
 import day from '../assets/day.jpg';
 import blacklogo from '../assets/logo-black.png';
@@ -7,12 +8,18 @@ import search from '../assets/search-b.png';
 import searchw from '../assets/search-w.png';
 import '../index.css';
 
-const Nav = () => {
+const Nav = ({ theme, setTheme }) => {
+
+  const toggle_mode = () => {
+    console.log("toggle_mode called!"); // Confirm function is called
+    setTheme(theme === 'light' ? 'dark' : 'light');
+    console.log("Theme is now:", theme); // Check theme *after* setting it
+  }
+
   return (
     <div className="navbar">
-
       <div>
-        <img src={blacklogo} alt="logo" className="logoo" />
+        <img src={theme === 'light' ? white : blacklogo} alt="logo" className="logoo" />
       </div>
       <div className="list">
         <ul className="list-items">
@@ -23,13 +30,12 @@ const Nav = () => {
         </ul>
       </div>
       <div className="search-box">
-        <img src={search} alt="" />
+        <img src={theme === 'light' ? search : searchw} className="search-icon" alt="img" />
         <input type="text" placeholder="Search..." />
       </div>
-
-      <img src={night} alt=".." />
+      <img onClick={toggle_mode} src={theme === 'light' ? night : day} className="dark" alt=".." />
     </div>
-  )
+  );
 }
 
 export default Nav;
